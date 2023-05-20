124bdeba938e81ae6e5b0617d31d4e40ede9d157
teletoph <go.cristopher@gmail.com>
2023-05-20 11:58:56 +0800

    Completed JS Exercises 5-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
-
+const sumAll = function(x,y) {
+    if (Number.isInteger(x) == false || Number.isInteger(y) == false)
+    {
+        return "ERROR";
+    }
+    if (x < 0 || y < 0)
+    {
+        return "ERROR";
+    }
+    let sum = 0;
+    let a = 0;
+    let b = 0;
+    if (x < y)
+    {
+        a = x;
+        b = y;
+    }
+    else 
+    {
+        a = y;
+        b = x;
+    }
+    for (let i = a; i < b + 1; i++)
+    {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  let cel = (far - 32) * 5 / 9 * 10;
+  cel = Math.round(cel) / 10;
+  return cel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let far = (cel * 9 / 5 + 32) * 10;
+  far = Math.round(far) / 10;
+  return far;
 };
 
 // Do not edit below this line
