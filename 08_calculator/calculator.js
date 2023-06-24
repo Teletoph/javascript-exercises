6232ede6ae4ab3ceaae696ab7984b06a77f54153
Cris <go.cristopher@gmail.com>
2023-06-24 17:01:17 +0800

    Finished 08_calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if(arr.length == 0)
+  {
+    return 0;
+  }
+  return arr.reduce((total, num) => total += num);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, num) => product = product * num, 1);  
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num == 0)
+  {
+    return 1;
+  }
+  let arr = [];
+  for (let i = num; i > 0; i--)
+  {
+    arr.push(i);
+  }
+  return arr.reduce((factorial, num) => factorial *= num, 1);
 };
+
 
 // Do not edit below this line
 module.exports = {
