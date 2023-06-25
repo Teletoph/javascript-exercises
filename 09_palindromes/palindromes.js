fe79878482470a1910999e3b648cbcd674564c23
Cris <go.cristopher@gmail.com>
2023-06-25 16:01:49 +0800

    Finished 09_Palindromes Exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,45 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function (string) {
+    let upperCaseString = string.toUpperCase();
+    let flatStr = [];
+    for(let i = 0; i < upperCaseString.length; i++)
+    {
+        if(upperCaseString[i].toUpperCase() != upperCaseString[i].toLowerCase())
+        {
+            flatStr.push(upperCaseString[i]);
+        }
+    }
+    if (flatStr.length % 2 == 0)
+    {
+        let arrA = flatStr.slice(0, flatStr.length / 2);
+        let arrB = flatStr.slice(flatStr / 2).reverse();
+        let count = 0;
+        for (let i = 0; i < arrA.length; i++)
+        {
+            if (arrA[i] == arrB[i]) {
+                count++;
+            }
+        }
+        if (count == arrA.length)
+        {
+            return true;
+        }
+        return false;
+    }
+    let divide = (flatStr.length - 1) / 2;
+    let arrA = flatStr.slice(0, divide);
+    let arrB = flatStr.slice(divide + 1).reverse();
+    let count = 0;
+    for (let i = 0; i < arrA.length; i++)
+    {
+        if (arrA[i] == arrB[i]) {
+            count++;
+        }
+    }
+    if (count == arrA.length)
+    {
+        return true;
+    }
+    return false;
+};  
 // Do not edit below this line
 module.exports = palindromes;
