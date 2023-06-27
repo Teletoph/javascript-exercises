f8d70b2e8d426c0ea53e747a57260cc82882c68a
Cris <go.cristopher@gmail.com>
2023-06-27 11:18:47 +0800

    Finished 10_Fibonacci Exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(reps) {
+   if (reps == 1 || reps == 2)
+   {
+    return 1;
+   }
+   if(reps < 0)
+   {
+    return "OOPS";
+   }
+   let arr = [1, 1];
+   
+   for (let i = 2; i < reps; i++)
+   {
+        let newNum = arr[arr.length - 1] + arr[arr.length - 2];
+        arr.push(newNum);
+   }
+   return arr[arr.length - 1];
 };
 
 // Do not edit below this line
