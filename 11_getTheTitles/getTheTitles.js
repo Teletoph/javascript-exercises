44cfed4ba97bab98d45a5fd93ed688bc147bf537
Cris <go.cristopher@gmail.com>
2023-06-27 11:26:03 +0800

    Finished 11_getTheTiltles Exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(obj) {
+    let arr = [];
+    obj.map((book) => {
+        arr.push(book['title']);
+    });
+    return arr;
 };
 
 // Do not edit below this line
