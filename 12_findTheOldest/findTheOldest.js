dfac278ab91a14d3e9ef88e82b7a1ab38d06373e
Cris <go.cristopher@gmail.com>
2023-06-29 11:18:13 +0800

    Finished 12_findTheOldest Exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest;
+    people.map(people => {
+        if (!people['yearOfDeath'])
+        {
+            return people['yearOfDeath'] = new Date().getFullYear();
+        }
+    });
+    people.sort((a, b) => {
+        if ((a['yearOfDeath'] - a['yearOfBirth']) < (b['yearOfDeath'] - b['yearOfBirth'])) 
+        {
+            return 1;
+        }
+        else {
+            return -1;
+        }
+    });
+    oldest = people[0];
+    return oldest;
 };
 
 // Do not edit below this line
